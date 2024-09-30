b5f4e866b4d1d4c95148133333f7d0ff4baddf2e
Myriam789 <myriam.sall@epitech.digital>
2024-09-30 16:13:12 +0200

    ex_06.js

---
+++ ex_06.js
@@ -0,0 +1,7 @@
+If banana count = 0 
+displayThisText("Oh no, there is no banana");
+        } elese if (bananaCount >0) {
+displayThisText("Yummy");
+        } else { 
+displayThisText("Do I owe you bananas");
+{
