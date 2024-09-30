ab6fda06344464a88ae52b2922bd930efa280399
Myriam789 <myriam.sall@epitech.digital>
2024-09-30 17:12:45 +0200

    ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,1 +1,6 @@
-
+displayThisText("The variable value is "+ "'" + input_var + "'" + ".");
+displayThisText("The type is " + "'" + typeof(input_var) + "'" + ".");
+if (input_var === 42)
+{
+	displayThisText("It is the meaning of life.")
+}
