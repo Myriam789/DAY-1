db77856fcc412cda19495b1517a3e28cee54d83f
Myriam789 <myriam.sall@epitech.digital>
2024-09-30 12:23:18 +0200

    ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,10 @@
+var bool = "true"
+document.write(bool + "<br/>");
+var string = "forty two"
+document.write(string + "<br/>");
+var integer = "42"
+document.write(integer +"<br/>");
+var float = "42.42"
+document.write(float + "<br/>");
+var this_is_nul = "nul"
+document.write(null +"<br/>");
