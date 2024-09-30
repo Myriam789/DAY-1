b7f7c03554d324f53fb69bb74d4f877a8127b32c
Myriam789 <myriam.sall@epitech.digital>
2024-09-30 14:26:08 +0200

    ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+displayThisText(typeof shoes);
+displayThisText(typeof false);
+displayThisText(typeof 2);
+displayThisText(typeof 100n);
+displayThisText(typeof Symbol() )
+displayThisText(typeof null)
+displayThisText(typeof $)
